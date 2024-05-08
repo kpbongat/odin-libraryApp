cd3447d2fef62c4a79fb59e3a9162cd39d1ce439
kpbongat <kpbongat.dev@gmail.com>
2024-05-08 14:21:51 +0800

    Add displayBook function to display books in DOM

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const library = [];
+const library = [{
+    title:'The Bible',
+    author:'God',
+    pages:1000,
+    isRead:true
+}];
 
 function Book() {
     this.title = title;
@@ -7,5 +12,33 @@ function Book() {
     this.isRead = isRead;
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${(isRead) ? 'already read' : 'not read yet'}`;
+    }
+}
+
+function addBook() {
+    
+}
+
+function displayBooks() {
+    const libraryContainer = document.querySelector('.library');
+    for (const book of library) {
+        const bookCard = document.createElement('div');
+        const title = document.createElement('div');
+        title.classList.toggle('title');
+        title.textContent = book.title;
+        bookCard.appendChild(title);
+        const author = document.createElement('div');
+        author.classList.toggle('author');
+        author.textContent = book.author;
+        bookCard.appendChild(author);
+        const pages = document.createElement('div');
+        pages.classList.toggle('pages');
+        pages.textContent = book.pages;
+        bookCard.appendChild(pages);
+        const read = document.createElement('div');
+        read.classList.toggle('read');
+        read.textContent = (book.isRead) ? 'already read' : 'not read yet';
+        bookCard.appendChild(read);
+        libraryContainer.appendChild(bookCard);
     }
 }
