2a369a8cf963d281470118fbb9a8c7d575fe88b2
kpbongat <kpbongat.dev@gmail.com>
2024-06-24 17:03:58 +0800

    Create ScreenController class for all DOM logic

--- js/script.js
+++ js/script.js
@@ -25,83 +25,91 @@ class Book {
     }
 }
 
-
-
-function displayBooks() {
-    const libraryContainer = document.querySelector('.library');
-    for (const book of library) {
-        if (book.display) {
-            continue
-        }
-        const bookCard = document.createElement('tr');
-        bookCard.setAttribute('data-id', book['data-id']);
-        const title = document.createElement('td');
-        title.classList.toggle('title');
-        title.textContent = book.title;
-        const removeButton = document.createElement('input');
-        removeButton.setAttribute('type','image');
-        removeButton.setAttribute('src','./images/close-thick.svg');
-        removeButton.classList.toggle('remove');
-        removeButton.addEventListener('click', () => {
-            library.forEach((i, idx) => {
-                if (i['data-id'] === book['data-id']) {
-                    library.splice(idx, 1);
-                }
-
-            });
-            libraryContainer.removeChild(bookCard);
+class ScreenController {
+    constructor () {
+        const newBookButton = document.querySelector('.new-book');
+        newBookButton.addEventListener('click', () => {
+            const sidebarRef = document.querySelector('.sidebar');
+            sidebarRef.classList.toggle('hide');
+            
         })
-
-        bookCard.appendChild(title);
-        title.appendChild(removeButton); 
-        const author = document.createElement('td');
-        author.classList.toggle('author');
-        author.textContent = book.author;
-        bookCard.appendChild(author);
-        const pages = document.createElement('td');
-        pages.classList.toggle('pages');
-        pages.textContent = book.pages;
-        bookCard.appendChild(pages);
-        const read = document.createElement('td');
-        read.classList.toggle('read');
-        bookCard.appendChild(read);
-        const readCheckBox = document.createElement('input');
-        readCheckBox.setAttribute('type','checkbox');
-        if (book.isRead) {
-            readCheckBox.setAttribute('checked', true);
-        }
-        readCheckBox.addEventListener('change', () => {
-            library.forEach((i, idx) => {
-                if (i['data-id'] === book['data-id']) {
-                    i.isRead = !i.isRead;
-                }
-
-            });     
+        
+        const submitBookButton = document.querySelector('.submit-book');
+        submitBookButton.addEventListener('click', e => {
+                e.preventDefault();
+                const form = document.querySelector('.sidebar form');
+                const title = document.querySelector('#title').value;
+                const author = document.querySelector('#author').value;
+                const pages = document.querySelector('#pages').value;
+                const isRead = (document.querySelector('fieldset :checked').value === 'true');
+                library.addBook(title, author, pages, isRead);
+                displayBooks();
+                form.reset();
+        
         })
-        read.appendChild(readCheckBox);
-        libraryContainer.appendChild(bookCard);
-        book.display = true;
     }
+
+    displayBooks() {
+        const libraryContainer = document.querySelector('.library');
+        for (const book of library) {
+            if (book.display) {
+                continue
+            }
+            const bookCard = document.createElement('tr');
+            bookCard.setAttribute('data-id', book['data-id']);
+            const title = document.createElement('td');
+            title.classList.toggle('title');
+            title.textContent = book.title;
+            const removeButton = document.createElement('input');
+            removeButton.setAttribute('type','image');
+            removeButton.setAttribute('src','./images/close-thick.svg');
+            removeButton.classList.toggle('remove');
+            removeButton.addEventListener('click', () => {
+                library.forEach((i, idx) => {
+                    if (i['data-id'] === book['data-id']) {
+                        library.splice(idx, 1);
+                    }
+    
+                });
+                libraryContainer.removeChild(bookCard);
+            })
+    
+            bookCard.appendChild(title);
+            title.appendChild(removeButton); 
+            const author = document.createElement('td');
+            author.classList.toggle('author');
+            author.textContent = book.author;
+            bookCard.appendChild(author);
+            const pages = document.createElement('td');
+            pages.classList.toggle('pages');
+            pages.textContent = book.pages;
+            bookCard.appendChild(pages);
+            const read = document.createElement('td');
+            read.classList.toggle('read');
+            bookCard.appendChild(read);
+            const readCheckBox = document.createElement('input');
+            readCheckBox.setAttribute('type','checkbox');
+            if (book.isRead) {
+                readCheckBox.setAttribute('checked', true);
+            }
+            readCheckBox.addEventListener('change', () => {
+                library.forEach((i, idx) => {
+                    if (i['data-id'] === book['data-id']) {
+                        i.isRead = !i.isRead;
+                    }
+    
+                });     
+            })
+            read.appendChild(readCheckBox);
+            libraryContainer.appendChild(bookCard);
+            book.display = true;
+        }
+    }
+
+
+
 }
 
-const newBookButton = document.querySelector('.new-book');
 
-newBookButton.addEventListener('click', () => {
-    const sidebarRef = document.querySelector('.sidebar');
-    sidebarRef.classList.toggle('hide');
-    
-})
+const screen = new ScreenController ();
 
-const submitBookButton = document.querySelector('.submit-book');
-submitBookButton.addEventListener('click', e => {
-        e.preventDefault();
-        const form = document.querySelector('.sidebar form');
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').value;
-        const pages = document.querySelector('#pages').value;
-        const isRead = (document.querySelector('fieldset :checked').value === 'true');
-        library.addBook(title, author, pages, isRead);
-        displayBooks();
-        form.reset();
-
-})
